0913fd40608ce0deb1b34ad7e9a1d615dcba16e2
DevExpressExampleBot <fakeEmail>
2022-08-24 16:51:25 +0400

    Source auto update [skip ci]

--- VB/HowToBindOLAP/MainWindow.xaml.vb
+++ VB/HowToBindOLAP/MainWindow.xaml.vb
@@ -1,37 +1,28 @@
-﻿Imports System.Windows
+Imports System.Windows
 Imports DevExpress.Xpf.PivotGrid
 
 Namespace HowToBindOLAP
-    Partial Public Class MainWindow
+
+    Public Partial Class MainWindow
         Inherits Window
 
         Public Sub New()
-            InitializeComponent()
+            Me.InitializeComponent()
         End Sub
+
         Private Sub Window_Loaded(ByVal sender As Object, ByVal e As RoutedEventArgs)
-            pivotGridControl1.OlapConnectionString = "Provider=msolap;" &
-                "Data Source=http://demos.devexpress.com/Services/OLAP/msmdpump.dll;" &
-                "Initial Catalog=Adventure Works DW Standard Edition;" &
-                "Cube Name=Adventure Works;"
-            pivotGridControl1.BeginUpdate()
-
+            Me.pivotGridControl1.OlapConnectionString = "Provider=msolap;" & "Data Source=http://demos.devexpress.com/Services/OLAP/msmdpump.dll;" & "Initial Catalog=Adventure Works DW Standard Edition;" & "Cube Name=Adventure Works;"
+            Me.pivotGridControl1.BeginUpdate()
             ' Create fields.
-            Dim fieldMeasuresInternetSalesAmount As New  _
-                PivotGridField("[Measures].[Internet Sales Amount]", FieldArea.DataArea)
+            Dim fieldMeasuresInternetSalesAmount As PivotGridField = New PivotGridField("[Measures].[Internet Sales Amount]", FieldArea.DataArea)
             fieldMeasuresInternetSalesAmount.Caption = "Internet Sales Amount"
-
-            Dim fieldCustomerCountryCountry As New  _
-                PivotGridField("[Customer].[Country].[Country]", FieldArea.RowArea)
+            Dim fieldCustomerCountryCountry As PivotGridField = New PivotGridField("[Customer].[Country].[Country]", FieldArea.RowArea)
             fieldCustomerCountryCountry.Caption = "Country"
-
-            Dim fieldDateFiscalYearFiscalYear As New  _
-                PivotGridField("[Date].[Fiscal Year].[Fiscal Year]", FieldArea.ColumnArea)
+            Dim fieldDateFiscalYearFiscalYear As PivotGridField = New PivotGridField("[Date].[Fiscal Year].[Fiscal Year]", FieldArea.ColumnArea)
             fieldDateFiscalYearFiscalYear.Caption = "Fiscal Year"
-
             ' Add fields to the PivotGridControl
-            pivotGridControl1.Fields.AddRange(fieldMeasuresInternetSalesAmount, _
-                                        fieldCustomerCountryCountry, fieldDateFiscalYearFiscalYear)
-            pivotGridControl1.EndUpdate()
+            Me.pivotGridControl1.Fields.AddRange(fieldMeasuresInternetSalesAmount, fieldCustomerCountryCountry, fieldDateFiscalYearFiscalYear)
+            Me.pivotGridControl1.EndUpdate()
         End Sub
     End Class
 End Namespace
